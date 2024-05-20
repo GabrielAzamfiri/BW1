b3e39b7f64c5fe907cf28ba2337fc307fee8fbf1
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2024-05-20 13:13:35 +0200

    timer fatto

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+/* timer countdown da 60 secondi  */
+let tempoRimanente = 60;
+const countdownElement = document.getElementById("countdown");
+
+function aggCountdown() {
+  /* decrementa il tempo rimanente */
+  tempoRimanente--;
+  /* aggiorna il tempo */
+  countdownElement.textContent = tempoRimanente;
+  /* se scade il tempo  */
+  if (tempoRimanente <= 0) {
+    clearInterval(intervalloCountdown);
+    countdownElement.textContent = "tempo scaduto";
+  }
+}
+
+/* impostiamo l'intervallo di aggiornamento del timer */
+const intervalloCountdown = setInterval(aggCountdown, 1000);
