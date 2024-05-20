5f50454c83230810e753592bf66f8e7895e50596
AlexDiGioia <digioia.alex2001@gmail.com>
2024-05-20 15:07:51 +0200

    prime prove

---
+++ assets/scriptFeedback.js
@@ -0,0 +1,19 @@
+
+
+window.onload = function () {
+
+    console.log("saaaaaaaaas")
+    const starsArea=document.getElementById("stars-area")
+    for (let i = 0; i < 10; i++) {
+        console.log("saaaaaaaaas")
+        const star=document.createElement("button")
+        star.classList.add("star")
+        star.innerText="saaaaaaaaaaaaaaas";
+        star.style.height=100;
+        star.style.width=100;
+        star.style.backgroundImage="url('../img/star.svg')";
+        starsArea.appendChild(star);
+        
+    }
+
+}
