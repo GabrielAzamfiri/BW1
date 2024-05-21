37bad7e3f4cb836c43a08d22f33af9e8c669494c
ariannaal <ariannaloreti25@gmail.com>
2024-05-21 12:12:44 +0200

    pulizia develop

--- assets/JS/feedback.js
+++ assets/JS/feedback.js
@@ -1,26 +1,3 @@
-const checkbox = document.getElementById("checkboxWP");
-const divButton = document.getElementById("buttonJs");
-const button = document.createElement("button");
-button.textContent = "PROCEED";
-button.classList.add("styleButtonUnchecked");
-divButton.appendChild(button);
-
-function check() {
-  button.classList.remove("styleButton", "styleButtonUnchecked");
-
-  if (checkbox.checked) {
-    button.classList.add("styleButton");
-  } else {
-    button.classList.add("styleButtonUnchecked");
-  }
-}
-
-checkbox.addEventListener("click", check);
-
-button.addEventListener(click, function () {
-  window.location.href = "Question.html";
-});
-
 
 // FEEDBACK PAGE 
 
