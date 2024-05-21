aa8d61ea5e5d9878bdb74e5b1f3418f9ac9ceb3f
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2024-05-21 16:07:02 +0200

    timer2

---
+++ scriptTimer2.js
@@ -0,0 +1,10 @@
+const countdownNumberEl = document.getElementById("countdown-number");
+let countdown = 60;
+
+countdownNumberEl.textContent = countdown;
+
+setInterval(function () {
+  countdown = --countdown <= 0 ? 60 : countdown;
+
+  countdownNumberEl.textContent = countdown;
+}, 1000);
