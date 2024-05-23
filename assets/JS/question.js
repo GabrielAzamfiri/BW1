d3db595ddfe92071e9ac5c62356be782a6b9a6b1
Federico Di Presa <federicodipresa94@gmail.com>
2024-05-23 12:05:43 +0200

    update

--- assets/JS/question.js
+++ assets/JS/question.js
@@ -5,13 +5,18 @@ const questions = [
     difficulty: "easy",
     question: "What does CPU stand for?",
     correct_answer: "Central Processing Unit",
-    incorrect_answers: ["Central Process Unit", "Computer Personal Unit", "Central Processor Unit"],
+    incorrect_answers: [
+      "Central Process Unit",
+      "Computer Personal Unit",
+      "Central Processor Unit",
+    ],
   },
   {
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question: "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
+    question:
+      "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
     correct_answer: "Final",
     incorrect_answers: ["Static", "Private", "Public"],
   },
@@ -27,7 +32,8 @@ const questions = [
     category: "Science: Computers",
     type: "boolean",
     difficulty: "easy",
-    question: "Pointers were not used in the original C programming language; they were added later on in C++.",
+    question:
+      "Pointers were not used in the original C programming language; they were added later on in C++.",
     correct_answer: "False",
     incorrect_answers: ["True"],
   },
@@ -35,7 +41,8 @@ const questions = [
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question: "What is the most preferred image format used for logos in the Wikimedia database?",
+    question:
+      "What is the most preferred image format used for logos in the Wikimedia database?",
     correct_answer: ".svg",
     incorrect_answers: [".png", ".jpeg", ".gif"],
   },
@@ -45,13 +52,18 @@ const questions = [
     difficulty: "easy",
     question: "In web design, what does CSS stand for?",
     correct_answer: "Cascading Style Sheet",
-    incorrect_answers: ["Counter Strike: Source", "Corrective Style Sheet", "Computer Style Sheet"],
+    incorrect_answers: [
+      "Counter Strike: Source",
+      "Corrective Style Sheet",
+      "Computer Style Sheet",
+    ],
   },
   {
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question: "What is the code name for the mobile operating system Android 7.0?",
+    question:
+      "What is the code name for the mobile operating system Android 7.0?",
     correct_answer: "Nougat",
     incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
   },
@@ -75,7 +87,8 @@ const questions = [
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question: "Which programming language shares its name with an island in Indonesia?",
+    question:
+      "Which programming language shares its name with an island in Indonesia?",
     correct_answer: "Java",
     incorrect_answers: ["Python", "C", "Jakarta"],
   },
@@ -87,7 +100,9 @@ let risultato = 0;
 const questionResults = () => {
   const domanda = document.getElementById("domanda");
   if (i < questions.length) {
-    const risposte = questions[i].correct_answer.concat("," + questions[i].incorrect_answers).split(","); // array di risposte
+    const risposte = questions[i].correct_answer
+      .concat("," + questions[i].incorrect_answers)
+      .split(","); // array di risposte
 
     const questionNumber = document.getElementById("questionNumber");
     questionNumber.innerHTML = `QUESTION ${
@@ -106,23 +121,26 @@ const questionResults = () => {
       } while (numeriRandom.includes(randomNumber));
       numeriRandom.push(randomNumber);
 
-      const contenitoreRisposte = document.getElementById("contenitoreRisposte");
+      const contenitoreRisposte = document.getElementById(
+        "contenitoreRisposte"
+      );
       const risposta = document.createElement("button");
       risposta.classList.add("btnRisposta");
       risposta.innerText = risposte[numeriRandom[index]]; //come index un numero casuale cosi le riposte sono in ordine casuale
       contenitoreRisposte.appendChild(risposta);
+      risposta.classList.add("btnColoreViola");
 
       risposta.addEventListener("click", () => {
         // al click della risposta, se corretta aggiungi 1 al risultato altrimenti niente
-        risposta.classList.remove("btnRisposta");
-        risposta.classList.add("btnColoreViola");
         if (risposta.innerText === questions[i].correct_answer) {
+          risposta.id = "btnVerde";
           console.log("risposta giusta");
           // al click della risposta, se corretta aggiungi 1 al risultato altrimenti niente
           risultato += 1;
           console.log("il risultato è:", risultato);
         } else {
           console.log("risposta sbagliata");
+          risposta.id = "btnRosso";
         }
         i += 1;
 
